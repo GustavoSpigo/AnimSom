751da7cbdf33a39317de859d384becd7fe92d921
Gustavo Gomes <gustavo@spigo.net>
2021-03-15 22:21:44 -0300

    Aula 2021-03-15

---
+++ Assets/Aula 2021-03-15/PlayNaFala.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayNaFala : MonoBehaviour
+{
+    public AudioSource SomQueVaiTocar;
+    public void DaPlayNoBaguio()
+    {
+        if (SomQueVaiTocar.isPlaying)
+        {
+            SomQueVaiTocar.Stop();
+        }
+        SomQueVaiTocar.Play();
+    }
+}
---
+++ Assets/SensorPorta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorPorta : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("aberto", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("aberto", false);
+        }
+    }
+}
