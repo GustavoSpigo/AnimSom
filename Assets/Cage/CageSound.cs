6783b15ec8430a357be54a141856a3b6100d1acc
Gustavo Gomes <gustavo@spigo.net>
2021-03-22 22:26:08 -0300

    Gandalf e melhor SFX

---
+++ Assets/Cage/CageSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageSound : MonoBehaviour
+{
+    public AudioSource tocadorDeSom;
+    public AudioClip somIaaawww;
+    public AudioClip somPim;
+    public void TocarIaaawww()
+    {
+        tocadorDeSom.Stop();
+        tocadorDeSom.clip = somIaaawww;
+        tocadorDeSom.volume = 1;
+        tocadorDeSom.Play();
+    }
+    public void TocarPim(float Volume)
+    {
+        tocadorDeSom.Stop();
+        tocadorDeSom.clip = somPim;
+        tocadorDeSom.volume = Volume;
+        tocadorDeSom.Play();
+    }
+}
---
+++ Assets/ChamaTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChamaTrigger : MonoBehaviour
+{
+    public string TagQueAtivaTrigger;
+    public string NomeDaTrigger;
+    public Animator anim;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(TagQueAtivaTrigger))
+        {
+            anim.SetTrigger(NomeDaTrigger);
+        }
+    }
+}
